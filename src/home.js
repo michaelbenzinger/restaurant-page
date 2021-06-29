b210a81371119ce899a86647aef6e82bd73a7888
Michael Benzinger <michaelbenzinger3@gmail.com>
2021-06-29 10:08:20 -0500

    Started Restaurant Project

---
+++ src/home.js
@@ -0,0 +1,3 @@
+export default function displayHome() {
+  console.log("Displaying home");
+}
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './meyerreset.css'
+import './style.css'
+import displayHome from './home.js'
+import displayContact from './contact.js'
+import displayMenu from './menu.js'
+
+displayHome();
+
+console.log("We are working!");
